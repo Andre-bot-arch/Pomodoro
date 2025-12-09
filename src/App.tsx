a1232f0a659ecb7df1f9b0d6dcb899a294e16c9d
André <andredev808@gmail.com>
2025-12-09 07:31:12 -0400

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import {Container} from './components/Container';
+import { CountDown } from './components/CountDown';
+import { Cycles } from './components/Cycles';
+import { DefaultInput } from './components/DefaultInput';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
+import './styles/global.css';
+import './styles/theme.css';
+
+export function App() {
+    return (
+        <>
+            <Container>
+                <Logo/>
+            </Container>
+
+            <Container>
+                <Menu/>
+            </Container>
+
+            <Container>
+                <CountDown/>
+            </Container>
+
+            <Container>
+                <form className="form">
+                    <div className="formRow">
+                        <DefaultInput
+                         labelText='task'
+                         id='meuInput'
+                         type='text'
+                         placeholder='insira o nome da tarefa'
+                         />
+                    </div>
+
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor </p>
+                    </div>
+
+                    <div className="formRow">
+                        <Cycles/>
+                    </div>
+                 
+                    <div className="formRow">
+                        <button>Enviar</button>
+                    </div>
+                </form>
+            </Container>
+
+        </>
+    );
+}
